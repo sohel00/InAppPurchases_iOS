69858a944ec145e72b2bbdff62f021b41379fd96
Sohel Dhengre <soheldhengre@gmail.com>
2018-03-16 15:34:02 +0530

    added inApp purchase

--- InAppPurchases_iOS/Services/PurchaseManager.swift
+++ InAppPurchases_iOS/Services/PurchaseManager.swift
@@ -9,9 +9,57 @@
 import Foundation
 import StoreKit
 
-class PurchaseManager: NSObject {
+class PurchaseManager: NSObject, SKProductsRequestDelegate, SKPaymentTransactionObserver {
+    
+    
     
     static let instance = PurchaseManager()
+    let IAP_REMOVE_ADS = "Nick.InAppPurchasesiOS.remove.ads"
+    var productRequest: SKProductsRequest!
+    var products = [SKProduct]()
     
+    func fetchProducts(){
+        let productIds = NSSet(object: IAP_REMOVE_ADS) as! Set<String>
+        productRequest = SKProductsRequest(productIdentifiers: productIds)
+        productRequest.delegate = self
+        productRequest.start()
+    }
+    
+    
+    func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
+        if response.products.count > 0 {
+            print(response.products.debugDescription)
+            products = response.products
+        }
+    }
+    
+    func purchaseRemoveAds(){
+        if SKPaymentQueue.canMakePayments() && products.count > 0 {
+            let removeAdsProduct = products[0]
+            let payment = SKPayment(product: removeAdsProduct)
+            SKPaymentQueue.default().add(self)
+            SKPaymentQueue.default().add(payment)
+        }
+    }
+    
+    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+        for transaction in transactions {
+            switch transaction.transactionState {
+            case .purchased:
+                SKPaymentQueue.default().finishTransaction(transaction)
+                if transaction.payment.productIdentifier == IAP_REMOVE_ADS {
+                    UserDefaults.standard.set(true, forKey: IAP_REMOVE_ADS)
+                }
+                break
+            case .failed:
+                SKPaymentQueue.default().finishTransaction(transaction)
+                break
+            case .restored:
+                SKPaymentQueue.default().finishTransaction(transaction)
+                break
+            default: break
+            }
+        }
+    }
     
 }
