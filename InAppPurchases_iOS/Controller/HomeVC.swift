4c0d2dbf30e7986adc73c20e222b869a65b6f3ec
Sohel Dhengre <soheldhengre@gmail.com>
2018-03-16 11:54:59 +0530

    project setup

---
+++ InAppPurchases_iOS/Controller/HomeVC.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  InAppPurchases_iOS
+//
+//  Created by Sohel Dhengre on 14/03/18.
+//  Copyright © 2018 Sohel Dhengre. All rights reserved.
+//
+
+import UIKit
+import GoogleMobileAds
+
+class HomeVC: UIViewController {
+
+    @IBOutlet weak var bannerView: GADBannerView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        bannerView.rootViewController = self
+        bannerView.load(GADRequest())
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+
+}
+
---
+++ InAppPurchases_iOS/Services/PurchaseManager.swift
@@ -0,0 +1,17 @@
+//
+//  PurchaseManager.swift
+//  InAppPurchases_iOS
+//
+//  Created by Sohel Dhengre on 14/03/18.
+//  Copyright © 2018 Sohel Dhengre. All rights reserved.
+//
+
+import Foundation
+import StoreKit
+
+class PurchaseManager: NSObject {
+    
+    static let instance = PurchaseManager()
+    
+    
+}
